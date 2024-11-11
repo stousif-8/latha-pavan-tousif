f9c84f4967e76d7d1d08ae467f1dadea081cb0e1
SyedTousif <stousif1020@gmail.com>
2024-11-11 17:13:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,39 +1,26 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.70"  # Use latest 3.x version, adjust as necessary
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}  # This is defined as an empty block, as required by azurerm
-}
-
-# Create a resource group
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "West Europe"
 }
 
-# Create an App Service plan
-resource "azurerm_service_plan" "example" {
-  name                = "example-service-plan"
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_app_service_plan" "example" {
+  name                = "example-appserviceplan"
   location            = azurerm_resource_group.example.location
-  os_type             = "Linux"
-  sku_name            = "P1v2"
+  resource_group_name = azurerm_resource_group.example.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
 }
 
-# Create a Linux web app
-resource "azurerm_linux_web_app" "example" {
-  name                = "example-web-app"
+resource "azurerm_app_service" "example" {
+  name                = "example-app-service"
+  location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_service_plan.example.location
-  service_plan_id     = azurerm_service_plan.example.id
+  app_service_plan_id = azurerm_app_service_plan.example.id
 
   site_config {
-    # Add configuration settings here if needed
+    dotnet_framework_version = "v4.0"
   }
 }
