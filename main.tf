e943dddb9b9baea352a5d618c9b61f00e66ca4ac
SyedTousif <stousif1020@gmail.com>
2024-11-11 17:18:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,11 +11,13 @@ resource "azurerm_service_plan" "example" {
   name                = "example-appserviceplan"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  sku_name            = "P1V3"
+   os_type             = "Linux"
+   sku_name = P1V3
 }
 
+
 resource "azurerm_app_service" "example" {
-  name                = "example-app-service"
+  name                = "deployment-app"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   app_service_plan_id = azurerm_service_plan.example.id
