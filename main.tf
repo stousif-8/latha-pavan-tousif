13ba5e9d09b74e16fa9a812a498dd9b800d2af01
SyedTousif <stousif1020@gmail.com>
2024-11-11 15:32:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,3 +14,9 @@ resource "azurerm_service_plan" "example" {
   os_type             = "Linux"
   sku_name            = "B1"  # Correctly enclosed in double quotes
 }
+resource "azurerm_linux_web_app" "example" {
+  name                = "example-webapp"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  service_plan_id     = azurerm_service_plan.appservice.id
+}
