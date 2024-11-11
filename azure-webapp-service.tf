8924c798f3548f62616e69887f6bd0f91d3c5e4b
stousif-8 <stousif1020@gmail.com>
2024-11-11 15:02:33 +0530

    latha=pavan=syed

---
+++ azure-webapp-service.tf
@@ -0,0 +1,10 @@
+resource "azurerm_app_service" "example" {
+  name                = "deployment-app"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_service_plan.example.id
+
+  site_config {
+    linux_fx_version = "NODE|16"
+  }
+}
---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name = "azuredemo-rg"
+  location = "South India"
+}
+
+resource "azurerm_service_plan" "example" {
+  name                = "example-appserviceplan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+   os_type             = "Linux"
+   sku_name = P1V3
+}
