d7a652ce07125671b200f2d76d7fc197e78f35d7
SyedTousif <stousif1020@gmail.com>
2024-11-11 15:24:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,10 +7,10 @@ resource "azurerm_resource_group" "example" {
   location = "South India"
 }
 
-resource "azurerm_service_plan" "example" {
-  name                = "example-appserviceplan"
+resource "azurerm_service_plan" "appservice" {
+  name                = "testing"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   os_type             = "Linux"
-  sku_name            = "P0V3"  # Correctly enclosed in double quotes
+  sku_name            = "B1"  # Correctly enclosed in double quotes
 }
