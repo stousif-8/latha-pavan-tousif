11d485c4d430372e917253d3f2482f2e897a459e
SyedTousif <stousif1020@gmail.com>
2024-11-11 17:15:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,12 +7,7 @@ resource "azurerm_app_service_plan" "example" {
   name                = "example-appserviceplan"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-
-  sku {
-    tier = "Standard"
-    size = "S1"
-  }
-}
+  sku_name = P1V3
 
 resource "azurerm_app_service" "example" {
   name                = "example-app-service"
