d76676e08417f32456b59a01b0727b682f7aac0b
SyedTousif <stousif1020@gmail.com>
2024-11-11 17:16:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,24 @@
+provider "azurerm" {
+  features {}
+}
+
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "West Europe"
 }
 
-resource "azurerm_app_service_plan" "example" {
+resource "azurerm_service_plan" "example" {
   name                = "example-appserviceplan"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  sku_name = P1V3
+  sku_name            = "P1V3"
+}
 
 resource "azurerm_app_service" "example" {
   name                = "example-app-service"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  app_service_plan_id = azurerm_app_service_plan.example.id
+  app_service_plan_id = azurerm_service_plan.example.id
 
   site_config {
     dotnet_framework_version = "v4.0"
