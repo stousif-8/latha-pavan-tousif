a628fee7e28b949bb172eb1df07aae9573222df7
SyedTousif <stousif1020@gmail.com>
2024-11-11 15:31:15 +0530

    Update azure-webapp-service.tf

--- azure-webapp-service.tf
+++ azure-webapp-service.tf
@@ -1,10 +1,1 @@
-resource "azurerm_app_service" "example" {
-  name                = "deployment-app-latha-syed-pavan"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  app_service_plan_id = azurerm_service_plan.example.id
 
-  site_config {
-    linux_fx_version = "NODE|18"
-  }
-}
